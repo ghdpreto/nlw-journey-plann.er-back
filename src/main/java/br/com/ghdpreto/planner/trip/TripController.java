473f840994c68e11abd1bc8c430cc633149cc78f
ghdpreto <ghdpreto@gmail.com>
2024-07-11 19:54:58 -0300

    feat: detalhes da trip

--- src/main/java/br/com/ghdpreto/planner/trip/TripController.java
+++ src/main/java/br/com/ghdpreto/planner/trip/TripController.java
@@ -1,5 +1,9 @@
 package br.com.ghdpreto.planner.trip;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ghdpreto.planner.participant.ParticipantService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("trips")
@@ -27,6 +34,18 @@ public class TripController {
         this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
 
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TripEntity> getTripDetails(@PathVariable UUID id) {
+
+        Optional<TripEntity> trip = this.tripRepository.findById(id);
+
+        /**
+         * Condicional if()
+         * trip.map(ResponseEntity::ok).
+         */
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }

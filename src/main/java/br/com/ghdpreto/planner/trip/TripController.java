0ecd5e3bdf6d9ae445ed1c2778b99a704311246e
ghdpreto <ghdpreto@gmail.com>
2024-07-12 22:24:40 -0300

    feat: atualizar trip

--- src/main/java/br/com/ghdpreto/planner/trip/TripController.java
+++ src/main/java/br/com/ghdpreto/planner/trip/TripController.java
@@ -1,7 +1,11 @@
 package br.com.ghdpreto.planner.trip;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
+
+import javax.swing.text.html.Option;
 
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,7 @@ import br.com.ghdpreto.planner.participant.ParticipantService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("trips")
@@ -46,6 +51,25 @@ public class TripController {
          * trip.map(ResponseEntity::ok).
          */
         return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<TripEntity> updateTrip(@PathVariable UUID id, @RequestBody TripRequestPayload payload) {
+
+        Optional<TripEntity> trip = this.tripRepository.findById(id);
+
+        if (trip.isPresent()) {
+            TripEntity rawTrip = trip.get();
+            rawTrip.setStartsAt(LocalDateTime.parse(payload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+            rawTrip.setEndsAt(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+            rawTrip.setDestination(payload.destination());
+
+            this.tripRepository.save(rawTrip);
+
+            return ResponseEntity.ok(rawTrip);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
